0f19ccbde767de551e29ebfb3f30bebaf548a38e
Ely0rda <nagami0lite@gmail.com>
2022-12-23 16:21:29 +0100

    adding images and static fileServer

--- cmd/main/routes.go
+++ cmd/main/routes.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/Ely0rda/bookings/pkg/config"
 	"github.com/Ely0rda/bookings/pkg/handlers"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
@@ -23,5 +24,25 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	FileServer(mux, "/static/", http.Dir("../../static"))
+
 	return mux
+}
+
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit any URL parameters.")
+	}
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+
+		fs := http.StripPrefix("/static", http.FileServer(root))
+		fs.ServeHTTP(w, r)
+	})
 }
